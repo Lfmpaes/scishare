cd79ef8b171ab8ad3bf4c234ce3d011618ae1661
Luiz Fernando Paes <lfmpaes@gmail.com>
2020-05-01 20:10:27 -0300

    Feed Post component addition

--- scishare-dev/src/App.js
+++ scishare-dev/src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/navbar";
+import FeedPost from "./components/feedpost";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="container-sm feedpost-container">
+        <FeedPost authorName="Lorem Ipsum" text="Olá!" />
+        <FeedPost authorName="Ronaldo" text="Bom dia" />
+        <FeedPost authorName="Kassadin" text="Kai'Sa" />
+        <FeedPost authorName="Gangplank" text="Sarah Fortune" />
+        <FeedPost authorName="Alune" text="Aphelios" />
+      </div>
     </div>
   );
 }
---
+++ scishare-dev/src/components/navbar.js
@@ -0,0 +1,60 @@
+import React from "react";
+import userIcon from "./user-icon.svg";
+
+class navbar extends React.Component {
+  // state = {  }
+  render() {
+    return (
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+        <a className="navbar-brand align-middle" href="#" id="siteName">
+          ScienceShare
+        </a>
+
+        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li className="nav-item align-middle">
+            <a className="nav-link" href="#">
+              Início
+            </a>
+          </li>
+        </ul>
+
+        <div class="dropdown">
+          <button
+            class="btn btn-secondary dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false">
+            <img
+              src={userIcon}
+              className="navbar-item"
+              alt="user"
+              id="navbar-usericon"
+            />
+          </button>
+          <div
+            class="dropdown-menu dropdown-menu-right"
+            aria-labelledby="dropdownMenuButton">
+            <h6 class="dropdown-header">Usuário</h6>
+            <a class="dropdown-item" href="#">
+              Perfil
+            </a>
+            <a class="dropdown-item" href="#">
+              Configurações
+            </a>
+            <a class="dropdown-item" href="#">
+              Ajuda
+            </a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">
+              Encerrar sessão
+            </a>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default navbar;
